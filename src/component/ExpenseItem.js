c3d9bba14daaf2dd01025dd48d7c90048d8de27d
Long Tran <trlong357@gmail.com>
2021-05-20 14:41:52 +0700

    33 + 34. Adding basic CSS

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>May 27th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$300.00</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
